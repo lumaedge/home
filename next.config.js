5cb0dcb5bf0ac69f43fec4fd9e2bef87d6412074
LumaEdge <lumaedge@users.noreply.github.com>
2026-07-08 13:16:02 +0200

    simplify PrismaClient, remove Turso adapter

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@libsql/client', '@prisma/adapter-libsql'],
-  },
-}
+const nextConfig = {}
 module.exports = nextConfig
